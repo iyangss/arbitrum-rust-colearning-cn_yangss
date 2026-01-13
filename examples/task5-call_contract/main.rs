5b099a7cf74be7c55af4b74fa41ef6a37fb3f97a
yangss <cn.yangss@gmail.com>
2026-01-13 16:16:12 +0800

    update task5

--- examples/task5-call_contract/main.rs
+++ examples/task5-call_contract/main.rs
@@ -3,28 +3,26 @@ use alloy::{
     providers::ProviderBuilder,
     sol,
 };
-use std::error::Error;
+use eyre::Result;
 
-// Generate the contract bindings for the ERC20 interface.
-sol! {
-   // The `rpc` attribute enables contract interaction via the provider.
-   #[sol(rpc)]
-   contract ERC20 {
-        function name() public view virtual override returns (string memory);
-        function symbol() public view virtual override returns (string memory);
-        function decimals() public view virtual override returns (uint8);
-        function totalSupply() public view virtual override returns (uint256);
-   }
-}
+// Codegen from ABI file to interact with the contract.
+sol!(
+    #[allow(missing_docs)]
+    #[sol(rpc)]
+    IERC20,
+    "examples/task5-call_contract/abi/IERC20.json"
+);
 
-const RPC_URL: &str = "https://arb-sepolia.g.alchemy.com/v2/5rAhwxvzPaAMyn2_aCsvu";
+const RPC_URL: &str = "https://arbitrum-sepolia-testnet.api.pocket.network";
 const CONTRACT_ADDRESS: Address = address!("0x047CD90411B7E601ed30a9A1af91b08c11298eD8");
 
 #[tokio::main]
-async fn main() -> Result<(), Box<dyn Error>> {
+async fn main() -> Result<()> {
     let rpc_url = RPC_URL.parse()?;
     let provider = ProviderBuilder::new().connect_http(rpc_url);
-    let contract = ERC20::new(CONTRACT_ADDRESS, provider);
+
+    // Create a contract instance.
+    let contract = IERC20::new(CONTRACT_ADDRESS, provider);
 
     let name = contract.name().call().await?;
     let symbol = contract.symbol().call().await?;
